51a949fe4b7a0b1fd89ad1de9bc3a9e114354ef1
Soheil Koushan <soheil.koushan@gmail.com>
2014-10-12 19:37:04 -0400

    Allow for section param in Articles query

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -10,7 +10,7 @@ module Api
     def query_params
       # this assumes that an article belongs to an artist and has an :artist_id
       # allowing us to filter by this
-      params.permit(:article_id, :title)
+      params.permit(:section)
     end
 
   end
