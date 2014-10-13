a7e04bdccb81d246c64ccabb5ba90fc0a9ad06e9
Soheil Koushan <soheil.koushan@gmail.com>
2014-10-13 01:55:07 -0400

    MongoLab setup

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,11 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#nyt_rails_#{Rails.env}"
+if ENV["MONGOLAB_URI"]
+  uri = URI.parse(ENV["MONGOLAB_URI"])
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV["MONGOLAB_URI"])
+  MongoMapper.database = uri.path.gsub(/^\//, '')
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "#nyt_rails_#{Rails.env}"
+end
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
